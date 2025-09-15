e1831b96572b654e36486b2dce9c74a5df01a670
Nikhil Isukapalli <nikhil.isukapalli@gmail.com>
2025-09-15 15:40:04 -0700

    Removed post request

--- app/api/buildings/route.js
+++ app/api/buildings/route.js
@@ -11,15 +11,3 @@ export async function GET(request) {
         return NextResponse.json({ error: 'Failed to fetch buildings' }, { status: 500 })
     }
 }
-
-export async function POST(request) {
-    try {
-        const { name, address, type } = await request.json()
-        const building = await prisma.building.create({ data: { name, address, type } })
-        return NextResponse.json(building)
-        
-    } catch (error) {
-        console.log("Failed to create building", error)
-        return NextResponse.json({ error: 'Failed to create building' }, { status: 500 })
-    }
-}
--- prisma/migrations/20250915025913_building_init/migration.sql
+++ prisma/migrations/20250915025913_building_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateEnum
-CREATE TYPE "public"."BuildingType" AS ENUM ('UNIV_APT', 'SUITE', 'PLAZA_PRIV', 'PLAZA_SHARED', 'DELUXE', 'CLASSIC');
+CREATE TYPE "public"."BuildingType" AS ENUM ('UNIV_APT', 'SUITE', 'PLAZA', 'DELUXE', 'CLASSIC');
 
 -- CreateTable
 CREATE TABLE "public"."Building" (
@@ -9,22 +9,12 @@ CREATE TABLE "public"."Building" (
     "type" "public"."BuildingType" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "uid" TEXT NOT NULL,
 
     CONSTRAINT "Building_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Building_uid_key" ON "public"."Building"("uid");
-
--- CreateIndex
 CREATE INDEX "Building_name_idx" ON "public"."Building"("name");
-
--- CreateIndex
 CREATE INDEX "Building_address_idx" ON "public"."Building"("address");
-
--- CreateIndex
 CREATE INDEX "Building_type_idx" ON "public"."Building"("type");
-
--- CreateIndex
 CREATE UNIQUE INDEX "Building_name_address_key" ON "public"."Building"("name", "address");
